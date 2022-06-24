2bf98570808852c7d119556689395578d7dcb95e
KennyOlsen <106634393+KennyOlsen@users.noreply.github.com>
2022-06-24 09:44:06 -0600

    Added methods

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const express = require('express');
+const app = express();
+
+
+app.get("/cars", (request, response) => {
+    console.log("Get cars");
+});
+
+app.get("/car", (request, response) => {
+    console.log("Get car");
+});
+
+app.post("/car", (request, response) => {
+    console.log("Post car");
+});
+
+
+app.delete("/car", (request, response) => {
+    console.log("Delete car");
+});
+
+app.put("/car", (request, response) => {
+    console.log("Put car");
+});
+
+app.patch("/car", (request, response) => {
+    console.log("Patch car");
+});
