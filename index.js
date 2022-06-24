972e5dd707e924550cbc4f632153a249a0bc86af
KennyOlsen <106634393+KennyOlsen@users.noreply.github.com>
2022-06-24 10:17:23 -0600

    Returns values

--- index.js
+++ index.js
@@ -11,26 +11,32 @@ const port = 8080;
 
 app.get("/cars", (request, response) => {
     console.log("Get cars");
+    response.send("Get Cars");
 });
 
 app.get("/car", (request, response) => {
     console.log("Get car");
+    response.send("Get Car");
 });
 
 app.post("/car", (request, response) => {
     console.log("Post car");
+    response.send("Post car");
 });
 
 app.delete("/car", (request, response) => {
     console.log("Delete car");
+    response.send("Delete car");
 });
 
 app.put("/car", (request, response) => {
     console.log("Put car");
+    response.send("Put car");
 });
 
 app.patch("/car", (request, response) => {
     console.log("Patch car");
+    response.send("Patch car");
 });
 
 
